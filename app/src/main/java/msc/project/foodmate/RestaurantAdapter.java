0385d2cb62a681968bb493793537b912fad8a877
Kymoraa <jmoraa55@gmail.com>
2018-07-14 18:35:43 +0100

    Commit #3 - 11/July/2018 @16:55 Restaurant upload working...

---
+++ app/src/main/java/msc/project/foodmate/RestaurantAdapter.java
@@ -0,0 +1,82 @@
+package msc.project.foodmate;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by Jackie Moraa on 7/12/2018.
+ */
+
+public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.ImageViewHolder> {
+
+    private Context mContext;
+    private List<CuisineUploads> mCuisineUploads;
+
+    public RestaurantAdapter(Context context, List<CuisineUploads> cuisineUploads){
+        mContext = context;
+        mCuisineUploads = cuisineUploads;
+    }
+
+    @Override
+    public ImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cuisine_items, parent, false);
+        return  new ImageViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ImageViewHolder holder, int position) {
+        CuisineUploads uploadCurrent = mCuisineUploads.get(position);
+//        Picasso.get()
+//                .load(uploadCurrent.getImageUri())
+//                .resize(50,50)
+//                .error(R.drawable.image_error)
+//                .into(holder.ivCuisine);
+
+//        Picasso picasso = new Picasso.Builder(mContext)
+//                .downloader(new )
+//                .build();
+
+        Glide.with(mContext)
+                .load(uploadCurrent.getImageUri())
+                .into(holder.ivCuisine);
+
+
+
+
+
+        holder.tvCuisineName.setText(uploadCurrent.getName());
+        holder.tvPrice.setText(uploadCurrent.getPrice());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCuisineUploads.size();
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView ivCuisine;
+        public TextView tvCuisineName, tvPrice;
+
+        public ImageViewHolder(View itemView) {
+            super(itemView);
+
+            ivCuisine = itemView.findViewById(R.id.ivCuisine);
+            tvCuisineName = itemView.findViewById(R.id.tvCuisineName);
+            tvPrice = itemView.findViewById(R.id.tvPrice);
+
+        }
+    }
+}
--- app/src/main/java/msc/project/foodmate/RestaurantHome.java
+++ app/src/main/java/msc/project/foodmate/RestaurantHome.java
@@ -1,12 +1,26 @@
 package msc.project.foodmate;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -28,6 +42,12 @@ public class RestaurantHome extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    private RecyclerView recyclerView;
+    private RestaurantAdapter restaurantAdapter;
+    private DatabaseReference databaseReference;
+    private List<CuisineUploads> mCuisineUploads;
+
 
     public RestaurantHome() {
         // Required empty public constructor
@@ -64,8 +84,41 @@ public class RestaurantHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.restaurant_home, container, false);
+        View view = inflater.inflate(R.layout.restaurant_home, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        mCuisineUploads= new ArrayList<>();
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("cuisineUploads");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                    CuisineUploads cuisineUploads = postSnapshot.getValue(CuisineUploads.class);
+                    mCuisineUploads.add(cuisineUploads);
+                }
+
+
+                restaurantAdapter = new RestaurantAdapter(getActivity(), mCuisineUploads);
+
+                recyclerView.setAdapter(restaurantAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getActivity(), databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        return view;
     }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -91,16 +144,6 @@ public class RestaurantHome extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
